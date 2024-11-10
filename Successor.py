9a0ac21ca9ee823db96ca6f2e4409ec26c5ce1b0
Mahsa <haghnevismahsa@gmail.com>
2024-11-11 00:33:27 +0330

    feat: Add checks for valid move positions in Successor function
    - Added bounds check for potential move positions.
    - Implemented condition to skip moves if the new position is an obstacle.
    - Ensured the player's next move does not collide with obstacles.

--- Successor.py
+++ Successor.py
@@ -4,6 +4,9 @@ def Successor(gameMap: list, playerLocation: tuple, direction: str, obstacles: l
     '''
     This function returns the successor of the current state based on the possible directions of the player.
     '''
+    
+    possible_moves = []
+    x , y = playerLocation
     # update the obstacles location
     gameMap, obstacles = OpstaclesUpdator(gameMap, obstacles)
     
@@ -15,4 +18,13 @@ def Successor(gameMap: list, playerLocation: tuple, direction: str, obstacles: l
         'right': (playerLocation[0], playerLocation[1] + 1)
     }
     
-    
+    # Create a list of potential moves, but we need to check various conditions
+    for direction , (new_x , new_y) in possibleMoves.items():
+        # Check if the new position is within bounds
+        if 0<=new_x<=len(gameMap) and 0<=new_y<=len(gameMap):
+            # Check if the new position is not an obstacle (at the player's next move)
+            if gameMap[new_x][new_y] == 'X':
+                # Check if the new position is an obstacle
+                continue
+            
+            
