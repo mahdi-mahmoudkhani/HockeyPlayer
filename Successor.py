806bb8a72fef53da5f58249ebf100a8731d3866b
Mahsa <haghnevismahsa@gmail.com>
2024-11-11 10:43:12 +0330

    refactor: Update Successor function with move validation and visited state tracking
    - Simplified directional move calculations.
    - Added checks for boundaries, obstacles, and previously visited states.
    - Ensured valid ball movements to non-obstructed cells.

--- Successor.py
+++ Successor.py
@@ -12,25 +12,33 @@ def Successor(gameMap, playerLocation, ballsLocations, obstacles, visited):
     gameMap, obstacles = OpstaclesUpdator(gameMap, obstacles)
     
     # Define potential moves with their corresponding locations
-    possibleMoves = {
-        'up': (playerLocation[0] - 1, playerLocation[1]),
-        'down': (playerLocation[0] + 1, playerLocation[1]),
-        'left': (playerLocation[0], playerLocation[1] - 1),
-        'right': (playerLocation[0], playerLocation[1] + 1)
-    }
+    possibleMoves = [("up", -1, 0), ("down", 1, 0), ("left", 0, -1), ("right", 0, 1)]
     
     # Create a list of potential moves, but we need to check various conditions
-    for direction , (new_x , new_y) in possibleMoves.items():
+    for direction , dx , dy in possibleMoves.items():
+        new_x = x + dx
+        new_y = y + dy
+        
         # Check if the new position is within bounds
-        if 0<=new_x<len(gameMap) and 0<=new_y<len(gameMap):
-            # Check if the new position is not an obstacle (at the player's next move)
-            if gameMap[new_x][new_y] != 'X':
-                # Check if the new position is not an obstacle
-                if (new_x , new_y) in ballsLocations :
-                    new_xBall = new_x + (new_x-playerLocation[0])
-                    new_yBall = new_y + (new_y - playerLocation[1])
-                    if gameMap[new_xBall][new_yBall] != 'X':
-                        possible_moves.append(gameMap[new_x][new_y])
+        if not( 0<=new_x<len(gameMap) and 0<= new_y< len(gameMap)):
+            continue
+        
+        # Check if the new position is not an obstacle (at the player's next move)
+        if gameMap[new_x][new_y] == 'X':
+            continue
+        #check if it was seen before
+        if (new_x , new_y) in visited :
+            continue
+        #if its not seen then add this step 
+        visitedNew = visited.copy()
+        visitedNew.add((new_x , new_y))
+        
+        
+        if (new_x , new_y) in ballsLocations :
+            new_xBall = new_x + (new_x-playerLocation[0])
+            new_yBall = new_y + (new_y - playerLocation[1])
+            if gameMap[new_xBall][new_yBall] != 'X':
+                possible_moves.append(gameMap[new_x][new_y])
                     
                     
     
