ff2edbac3090792422a694091e82b37a06c68a71
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-11-11 20:53:59 +0330

    refactor(game): update heuristic function to use MapState class
    - Refactors heuristic function to work with MapState class attributes instead of separate lists
    - Calculates heuristic based on Manhattan distances between player, balls, and goals from MapState
    - Improves code readability and integration with MapState object structure

--- Heuristic.py
+++ Heuristic.py
@@ -1,13 +1,15 @@
 from manhattanDist import manhattanDistance
-def heuristic(gameMap: list, playerLocation: tuple, ballsLocations: list, goalLocations: list):
+from MapHandler import *
+
+def heuristic(mapState: MapState):
     '''
     This function returns the heuristic value of the current
     state based on the manhattan distance between the player and the closest ball + the ball and the closest goal.
     '''
     
      # get the manhattan distance between the player and the closest ball
-    playerToBall = min([manhattanDistance(playerLocation, ball) for ball in ballsLocations
-                         if ball not in goalLocations], default=None)
-    ballToGoal = min([manhattanDistance(ball, goal) for ball in ballsLocations
-        if ball not in goalLocations], default=None)
+    playerToBall = min([manhattanDistance(mapState.playerLocation, ball) for ball in mapState.ballsLocations
+                         if ball not in mapState.goalLocations], default=None)
+    ballToGoal = min([manhattanDistance(ball, mapState.goal) for ball in mapState.ballsLocations
+        if ball not in mapState.goalLocations], default=None)
     return playerToBall + ballToGoal
