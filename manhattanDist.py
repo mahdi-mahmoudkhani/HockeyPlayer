645fef26d3cc754a6bf62ddd58c6b15b8ca42701
Mahsa <haghnevismahsa@gmail.com>
2024-11-11 14:30:47 +0330

    feat: Add manhattanDistance function for calculating distance between points
    - Implemented a utility function to compute Manhattan distance between two points.
    - Returns the sum of the absolute differences between the coordinates of the points.

---
+++ manhattanDist.py
@@ -0,0 +1,5 @@
+def manhattanDistance(point1: tuple, point2: tuple):
+    '''
+    This function returns the manhattan distance between two points.
+    '''
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
