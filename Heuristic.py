db667b7e2b04dbf3e5cc6febee87f4be43ccc5f5
Mahsa <haghnevismahsa@gmail.com>
2024-11-11 17:24:23 +0330

    feat : Calculate heuristic distance from ball to goal
    - Ensures that the heuristic considers both player-to-ball and ball-to-goal distances.
    - Returns a combined distance to optimize A* pathfinding decisions.

--- Heuristic.py
+++ Heuristic.py
@@ -8,4 +8,6 @@ def heuristic(gameMap: list, playerLocation: tuple, ballsLocations: list, goalLo
      # get the manhattan distance between the player and the closest ball
     playerToBall = min([manhattanDistance(playerLocation, ball) for ball in ballsLocations
                          if ball not in goalLocations], default=None)
-    
+    ballToGoal = min([manhattanDistance(ball, goal) for ball in ballsLocations
+        if ball not in goalLocations], default=None)
+    return playerToBall + ballToGoal
