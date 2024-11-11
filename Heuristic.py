6e8cf83ede99240274dfdb33e89bb81be55d3ed0
Mahsa <haghnevismahsa@gmail.com>
2024-11-11 17:14:05 +0330

    feat: Add heuristic function to estimate state value
    - Added heuristic function to calculate estimated cost for a given game state.
    - Uses manhattanDistance between player and closest ball, and between ball and closest goal.

---
+++ Heuristic.py
@@ -0,0 +1,11 @@
+
+def heuristic(gameMap: list, playerLocation: tuple, ballsLocations: list, goalLocations: list):
+    '''
+    This function returns the heuristic value of the current
+    state based on the manhattan distance between the player and the closest ball + the ball and the closest goal.
+    '''
+    
+     # get the manhattan distance between the player and the closest ball
+    playerToBall = min([manhattanDistance(playerLocation, ball) for ball in ballsLocations
+                         if ball not in goalLocations], default=None)
+    
