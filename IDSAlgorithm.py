6d36e23a4ed6b4aa577910d9386812fcb9a200cf
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-11-13 01:23:22 +0330

    feat(IDSAlgorithm): enhance iterative deepening with max depth limit
    - Added `maxLimit` parameter with default `float('inf')` to IDSAlgorithm for flexible depth control.
    - Modified IDS to iteratively call DLSAlgorithm, incrementing depth until solution found or max limit reached.
    - Ensures controlled depth increase, improving algorithm efficiency within specified bounds.

---
+++ IDSAlgorithm.py
@@ -0,0 +1,17 @@
+from MapHandler import MapState
+from DLSAlgorithm import DLSAlgorithm
+
+def IDSAlgorithm(mapState: MapState, maxLimit = float('inf')):
+    '''
+    This function is the implementation of the Iterative Deepening Search Algorithm.
+    It will call the DLSAlgorithm function with increasing depth limits until the solution is found or the maxLimit is reached.
+    The maxLimit is entered by the user and is the maximum depth the algorithm will search to.
+    '''
+    depth = 0
+    while depth <= maxLimit:
+        result = DLSAlgorithm(mapState, depth)
+        if result is not None:
+            return result
+        depth += 1
+
+    return None
