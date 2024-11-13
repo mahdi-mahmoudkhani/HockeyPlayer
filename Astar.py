e39b8e73d3b9bb947b05a2b57f9edc7cfb5aa83e
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 22:35:34 +0330

    fix : fixed A star function tuples
     - fixed a star functions queue input

--- Astar.py
+++ Astar.py
@@ -3,28 +3,32 @@ from Successor import Successor
 from MapHandler import MapState
 from queue import PriorityQueue
 from Heuristic import heuristic
-def aStarSearch(initialState: MapState):
-    visited  = set()
-    frontier = PriorityQueue()
-    frontier.put((0,initialState))
-    visited.add((initialState.playerLocation , tuple(initialState.ballsLocations)))
+def aStarSearch(initialState):
+    visited = set()
+    frontier = PriorityQueue()  
+    frontier.put((0, initialState)) 
+    visited.add((initialState.playerLocation, tuple(initialState.ballsLocations)))
     
     expandedNodes = 0
     
     while not frontier.empty():
-        cost , currState = frontier.get()
+        cost, currState = frontier.get()  
         expandedNodes += 1
+        
+        
         if isGoalState(currState):
             total_cost = currState.cost + int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])
-            steps = currState.depth+1
-            return currState
+            steps = currState.depth + 1
+            
+            return currState 
         
-        successors = Successor(currState,visited)
         
-        for succesor in successors:
-            if  (successor.playerLocation, tuple(successor.ballsLocations)) not in visited:
+        successors = Successor(currState, visited)
+        
+        for successor in successors:
+            if (successor.playerLocation, tuple(successor.ballsLocations)) not in visited:
                 visited.add((successor.playerLocation, tuple(successor.ballsLocations)))
-                totalCost = heuristic(succesor) + succesor.cost
-                frontier.put((totalCost,succesor))
+                totalCost = heuristic(successor) + successor.cost
+                frontier.put((totalCost, successor)) 
             
     return None  # If no solution is found
