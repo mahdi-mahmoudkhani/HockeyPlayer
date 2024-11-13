a2eea67348a608b4f0e445ff028159c5e0d97a20
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 22:46:50 +0330

    feat : added visited states
    - added visited states
    - check if successor has been visited or not

--- IDAstar.py
+++ IDAstar.py
@@ -4,25 +4,29 @@ from Successor import Successor
 from GoalStateChecker import isGoalState
 from Heuristic import heuristic
 
-def IDAstarSearch(initialState : MapState):
-    def Search(currState , cost , limit):
+def IDAstarSearch(initialState: MapState):
+    visited = set()
+
+    def Search(currState, cost, limit):
         if isGoalState(currState):
             currState.cost += int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])
-            return currState , True
+            return currState, True
         if cost > limit:
-            return None , False # not found
+            return None, False  # not found
         newCost = float('inf')
-        successors = Successor(currState)
+        successors = Successor(currState, visited)  
         for successor in successors:
-            newCost = cost + heuristic(successor) + successor.cost
-            result , found = Search(successor , newCost , limit)
+            if successor not in visited :
+                visited.add((successor.playerLocation, tuple(successor.ballsLocations)))
+                newCost = cost + heuristic(successor) + successor.cost
+                result, found = Search(successor, newCost, limit)
             if found:
-                return result , True
-        return None , False # not found
-    
+                return result, True
+        return None, False  # not found
+
     limit = heuristic(initialState)
     while True:
-        result , found = Search(initialState , 0 , limit)
+        result, found = Search(initialState, 0, limit)
         if found:
             return result
         limit += 1
