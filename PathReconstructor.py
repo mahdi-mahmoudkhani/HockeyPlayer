417ea2016a9121b9885a31a4feab58bc73f2bdec
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 18:33:53 +0330

    feat :add path reconstruction with capitalized moves
    - Capitalized the first letter of each move for improved readability.

--- PathReconstructor.py
+++ PathReconstructor.py
@@ -1,6 +1,6 @@
 def reconstruct_path(state):
     path = []
     while state.parent is not None:
-        path.append(state.move)
+        path.append(state.move[0].capitalize())
         state = state.parent
     return path[::-1]
