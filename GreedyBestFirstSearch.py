40bae184dd430fbd5f178864105198dff30b60a4
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 22:39:17 +0330

    feat : added parent of the currState
    - Correctly track cost and depth for successor states in greedyBestFirstSearch.
    - Added parent reference for proper state traversal.
    - Updated frontier insertion to reflect accurate state costs based on heuristics.
    - Removed duplicate loop over successors.

--- GreedyBestFirstSearch.py
+++ GreedyBestFirstSearch.py
@@ -2,31 +2,35 @@ from MapHandler import MapState
 from Successor import Successor
 from GoalStateChecker import isGoalState
 from queue import PriorityQueue
-from Heuristic import heuristic
-
+from Heuristic import heuristic 
 def greedyBestFirstSearch(initialState: MapState):
     visited  = set()
     frontier = PriorityQueue()
     frontier.put((0,initialState))
     visited.add(initialState.playerLocation)
     
-    
     while not frontier.empty():
-        cost , currState = frontier.get()
+        cost, currState = frontier.get()
+        
         if isGoalState(currState):
+            
             currState.cost += int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])
             currState.depth += 1
             return currState
         
-        successors = Successor(currState,visited)
+        successors = Successor(currState, visited)
         
-        for succesor in successors :
-            if succesor.playerLocation not in visited:
-                visited.add(succesor.playerLocation)
-                totalCost = cost + int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0]) + heuristic(succesor)
-                frontier.put((totalCost ,succesor))
-        
+        for successor in successors:
+            if successor.playerLocation not in visited:
+                visited.add(successor.playerLocation)
+                totalCost = heuristic(successor)  
+                successor.parent = currState  
+                successor.cost = currState.cost + int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])  
+                successor.depth = currState.depth + 1  
+                
+                frontier.put((totalCost, successor))
+                
     return None
-               
+    
         
 
