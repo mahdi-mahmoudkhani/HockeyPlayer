8b9687f6514c5e51a55cf5fbc3c5812d9a30778f
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 17:02:37 +0330

    feat : added goal state check and conditions
    - retrieve current state with cost from frontier queue
    - add cost at player’s location to the total cost if goal state is reached
    - increment depth when goal state is found and return the current state

--- GreedyBestFirstSearch.py
+++ GreedyBestFirstSearch.py
@@ -9,4 +9,10 @@ def greedyBestFirstSearch(initialState: MapState):
     frontier.put((0,initialState))
     visited.add(initialState.playerLocation)
     
+    while not frontier.empty():
+        cost , currState = frontier.get()
+        if isGoalState(currState):
+            currState.cost += int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])
+            currState.depth += 1
+            return currState
 
