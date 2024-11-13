d90374da49f37c1c0537324d899abecf26520c8e
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 20:52:43 +0330

    fix : Changed ballToGoal logic
    - Fixed goal iterations in ballToGoal list comprehension

--- Heuristic.py
+++ Heuristic.py
@@ -6,10 +6,12 @@ def heuristic(mapState: MapState):
     This function returns the heuristic value of the current
     state based on the manhattan distance between the player and the closest ball + the ball and the closest goal.
     '''
-    
      # get the manhattan distance between the player and the closest ball
     playerToBall = min([manhattanDistance(mapState.playerLocation, ball) for ball in mapState.ballsLocations
                          if ball not in mapState.goalLocations], default=None)
-    ballToGoal = min([manhattanDistance(ball, mapState.goal) for ball in mapState.ballsLocations
-        if ball not in mapState.goalLocations], default=None)
+    ballToGoal =min([manhattanDistance(ball, goal) 
+                      for ball in mapState.ballsLocations 
+                      for goal in mapState.goalLocations
+                      if ball not in mapState.goalLocations], default=None)
+
     return playerToBall + ballToGoal
