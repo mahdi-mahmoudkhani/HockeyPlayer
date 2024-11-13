a1d3886ea9334f2a1e9e99894c1b3649b7c8bee0
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 16:35:50 +0330

    feat : update frontier with successors based on cost and location
    - iterate through successors and add them to visited if not previously visited
    - add successors to frontier with updated cost

--- Astar.py
+++ Astar.py
@@ -16,7 +16,12 @@ def aStarSearch(initialState: MapState):
             currState.cost += int(currState.gameMap[currState.playerLocation[0]][currState.playerLocation[1]][0])
             currState.depth += 1
             return currState
-       
+        for Successor in Successor(currState,visited):
+            if Successor.playerLocation not in visited:
+                visited.add(Successor.playerLocation)
+                frontier.put((Successor.cost +int(gameMap[playerLocation[0]][playerLocation[1]][0]),Successor))
+            
+            
         
     return None
     
