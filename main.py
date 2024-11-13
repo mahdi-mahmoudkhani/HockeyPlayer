107eb090024cd5bafcacd9f73bb083d4f4732793
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 19:45:39 +0330

    feat : Created main.py file with important algorithms import
    - import search algorithms (BFS, DFS, UCS, IDS, A*, Greedy Best First, IDA*) into the main file for pathfinding functionality
    - build main file in order to show all data on it

---
+++ main.py
@@ -0,0 +1,11 @@
+from MapHandler import MapState
+from BFSAlgorithm import BFSAlgorithm
+from DFSAlgorithm import DFSAlgorithm
+from UCSAlgorithm import UCSAlgorithm
+from IDSAlgorithm import IDSAlgorithm
+from Astar import aStarSearch
+from GreedyBestFirstSearch import greedyBestFirstSearch
+from IDAstar import IDAstarSearch
+from PathReconstructor import reconstruct_path
+
+
