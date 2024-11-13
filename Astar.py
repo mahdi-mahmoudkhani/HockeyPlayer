740412567e680fe9a113b80c5a43cc11696d70e7
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 11:55:47 +0330

    build : Initialize A* search with frontier queue and visited set
    - Set up the initial state in the priority queue
    - Track the player's location in the visited set
    - Initialize counter for expanded nodes

---
+++ Astar.py
@@ -0,0 +1,10 @@
+
+def aStarSearch(initialState: MapState):
+    visited  = set()
+    frontier = PriorityQueue()
+    frontier.put((0,initialState))
+    visited.add(initialState.playerLocation)
+    
+    expandedNodes = 1
+    
+    
