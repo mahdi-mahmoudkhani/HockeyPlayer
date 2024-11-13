2c5615cd9b37ad266072f766d0f313b638e66ea4
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 14:13:51 +0330

    feat: implement main loop structure for A* search
    - add loop to expand nodes while frontier is not empty
    - return None if no solution is found

--- Astar.py
+++ Astar.py
@@ -5,6 +5,13 @@ def aStarSearch(initialState: MapState):
     frontier.put((0,initialState))
     visited.add(initialState.playerLocation)
     
-    expandedNodes = 1
+    expandedNodes = 0
+    
+    while not frontier.empty():
+        cost , currState = frontier.get()
+        expandedNodes += 1
+        
+        
+    return None
     
     
