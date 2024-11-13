0e46981e5dfa9b78ab402ad6db6a9f10ce7f8cb5
Mahsa <haghnevismahsa@gmail.com>
2024-11-13 16:59:59 +0330

    feat‌ : added visited set and frontier queue
    - create visited set to track explored nodes
    - initialize frontier priority queue
    - mark initial player location as visited

--- GreedyBestFirstSearch.py
+++ GreedyBestFirstSearch.py
@@ -1,6 +1,12 @@
 from MapHandler import MapState
 from Successor import Successor
 from GoalStateChecker import isGoalState
+from queue import PriorityQueue
 
 def greedyBestFirstSearch(initialState: MapState):
+    visited  = set()
+    frontier = PriorityQueue()
+    frontier.put((0,initialState))
+    visited.add(initialState.playerLocation)
     
+
